7e640974d7523c04af1053e12e9bd4d144c83ab7
Sergey Yavorsky <yavorskyserge@gmail.com>
2023-12-07 18:55:03 +0300

    two save

--- women/views.py
+++ women/views.py
@@ -1,30 +1,38 @@
-from django.http import Http404, HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.http import (
+    Http404,
+    HttpRequest,
+    HttpResponse,
+    HttpResponseNotFound,
+    HttpResponsePermanentRedirect,
+    HttpResponseRedirect,
+)
+from django.shortcuts import redirect, render, reverse
 
 
 # Create your views here.
-def index(request):
+def index(request: HttpRequest):
     return HttpResponse(b"hello gigachad")
 
 
-def categories(request, cat_id):
+def categories(request: HttpRequest, cat_id):
     return HttpResponse(f"<h1>cats a lot cats!!!</h1>"
                         f"<p>id: {cat_id}</p>")
 
 
-def categories_by_slug(request, cat_slug):
+def categories_by_slug(request: HttpRequest, cat_slug):
     if request.GET:
         print(request.GET)
     return HttpResponse(f"<h1>many a lot of slugs!!!</h1>"
                         f"<p>slug: {cat_slug}</p>")
 
 
-def archive(request, year):
+def archive(request: HttpRequest, year):
     if year > 2023:
-        raise Http404()
+        uri = reverse('cats', args=('music', ))
+        return HttpResponsePermanentRedirect(uri)
     return HttpResponse(f"<h1>Архив по годам</h1>"
                         f"<p>year: {year}")
 
 
-def page_not_found(request, exception):
+def page_not_found(request: HttpRequest, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
