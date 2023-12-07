3b067ab44acad9a7c08da9928f54b9d7ce178e21
Sergey Yavorsky <yavorskyserge@gmail.com>
2023-12-07 17:42:52 +0300

    save

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lla9ll_+wvyw-76uf1ogxtf^7^7++@@84g=l#hxic)yzi60*r0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- women/views.py
+++ women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -13,10 +13,18 @@ def categories(request, cat_id):
 
 
 def categories_by_slug(request, cat_slug):
+    if request.GET:
+        print(request.GET)
     return HttpResponse(f"<h1>many a lot of slugs!!!</h1>"
                         f"<p>slug: {cat_slug}</p>")
 
 
 def archive(request, year):
+    if year > 2023:
+        raise Http404()
     return HttpResponse(f"<h1>Архив по годам</h1>"
                         f"<p>year: {year}")
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
